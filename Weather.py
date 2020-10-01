f0c257f1b8bf4f71a6fcad949a4173908000dd67
Yashdew <yashdewangan123456@gmail.com>
2020-10-01 23:16:45 +0530

    added check weather feature

--- Weather.py
+++ Weather.py
@@ -1,1 +1,35 @@
-print("Hello World!")
+from flask import Flask, render_template, request
+import os
+
+import requests
+app = Flask(__name__)
+
+
+
+url='http://api.openweathermap.org/data/2.5/weather?q={}&appid=<ENTER_YOUR_API_KEY>' #
+
+
+
+@app.route('/',methods=['GET', 'POST'])
+def index():
+   CITY=request.form.get('city')
+   r = requests.get(url.format(CITY)).json()
+   print(CITY)
+   print(r)
+   weather ={
+            'city' : CITY,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon']
+
+            
+        }
+   #return render_template("result.html",)
+   return render_template("index.html",weather=weather)
+
+"""@app.route('/show',methods=['GET', 'POST'])
+def result():"""
+  
+
+if __name__ == '__main__':
+    app.run(debug=True)
